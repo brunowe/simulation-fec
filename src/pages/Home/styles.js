b4f3c13c0868dac1984a94db8f255a5e291178f8
Bruno Weber de Castro <brunoweber500@gmail.com>
2020-04-21 11:49:05 -0300

    STYLE / Estiliza div da pag home

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,14 @@
+/* --------------------------------- IMPORTS ---------------------------------*/
+import styled from "styled-components";
+
+/* --------------------------------- EXPORTS ---------------------------------*/
+
+export const P5 = styled.div`
+  display: block;
+  height: 600px;
+  width: 50%;
+  background-color: #ddd;
+  border: solid 1px #aaa;
+  border-radius: 4px;
+  margin: auto;
+`;
